b7cb27bec98df5202bb49af56d1f5cfdcb06146a
rabbitverse-commits <rabbitverse@gmail.com>
2025-10-22 21:51:18 -0700

    week4-assignments1-5 (STARTED: 10% COMPLETE)

--- Week 4 assignment/assignment 4/problem1.js
+++ Week 4 assignment/assignment 4/problem1.js
@@ -13,8 +13,12 @@ function fahrenheitToCelsius(fahrenheit) {
     // Convert Fahrenheit to Celsius
     // Formula: (F - 32) × 5/9
     //Copied from slides because Math is hard.
-    let sum = num1 + num2 + num3;
-    return sum / 3;
+
+    //let sum = num1 + num2 + num3;
+    //return sum / 3;
+    
+    //Nevermind
+    return (fahrenheit - 32) * 5/9;
 }
 
 function getTemperatureDescription(fahrenheit) {
@@ -23,23 +27,28 @@ function getTemperatureDescription(fahrenheit) {
     
     // Below 32: "Freezing"
     if (temperature < 32){
-        console.log("It's freezing!");
+        //console.log//
+        return "It's freezing!";
     }  
         // 32-50: "Cold"
         else if (temperature <= 50){
-        console.log("It's cold!");
+        //console.log//
+        return ("It's cold!");
     }
         // 51-70: "Cool"
          else if (temperature <= 70){
-        console.log("It's cool.");
+         //console.log//
+         return  ("It's cool.");
     }
         // 71-85: "Warm"
         else if (temperature <= 85){
-        console.log("It's warm.");
+         //console.log//
+         return  ("It's warm.");
     }
         // Above 85: "Hot"
         else if (temperature > 80){
-        console.log("It's hot!");
+         //console.log//
+         return ("It's hot!");
     }
 
 }
