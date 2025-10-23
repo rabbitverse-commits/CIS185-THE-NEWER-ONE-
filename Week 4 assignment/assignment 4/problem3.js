7d448c1c22d19c5a1f411b100af15f742c3335c5
rabbitverse-commits <rabbitverse@gmail.com>
2025-10-22 23:07:42 -0700

    week4-assignments1-5 (STARTED: 30% COMPLETE)

--- Week 4 assignment/assignment 4/problem3.js
+++ Week 4 assignment/assignment 4/problem3.js
@@ -1,6 +1,6 @@
 // Problem 3: Grade Calculator
 // Marian Bambaloff
-// October 22, 2015
+// October 22-23, 2015
 
 function calculateAverage(scores) {
     // Calculate and return the average of an array of scores
@@ -9,7 +9,9 @@ function calculateAverage(scores) {
         let sum = num1 + num2 + num3;
         return sum / 3;
     }
-    
+    for (let i=0; i < average; i++) {
+        console.log(average[i]);
+    }
 
 }
 
@@ -17,13 +19,40 @@ function dropLowestScore(scores) {
     // Return a new array with the lowest score removed
     // If multiple lowest scores exist, remove only one
     // Don't modify the original array!
-
+    for (let i = 0; i > 0; i--) {
+        console.log(i);
+    }
 
 }
 
 function getLetterGrade(score) {
     // Return letter grade based on score
-
+    // Below 50 = F, right?
+        if (getLetterGrade <= 50){
+         //console.log//
+         return ("F");
+    }  
+        // 60 = D, right?
+        else if (getLetterGrade = 60){
+         //console.log//
+         return ("D");
+    }
+        // 70 = C, right?
+        else if (getLetterGrade = 70){
+         //console.log//
+         return ("C");
+    }
+        // 80 = B, right?
+        else if (getLetterGrade = 80){
+         //console.log//
+         return ("B");
+    }
+        // 90 = A, right?
+        else if (getLetterGrade >= 90){
+         //console.log//
+         return ("A");
+        }
+    return getLetterGrade(score);
 
 }
 
